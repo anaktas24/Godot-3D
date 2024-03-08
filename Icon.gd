5856f291b49729d85159fb4d7e9cfd3638db7d7c
Ioannis <72883899+anaktas24@users.noreply.github.com>
2024-03-08 21:00:48 +0100

    created coin

---
+++ Icon.gd
@@ -0,0 +1,15 @@
+extends Sprite3D
+
+var coins  =5 	
+var player_name = "robot"
+var hearts = 3.5
+const speed = 5
+var x = coins/speed
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotate_y(deg_to_rad(speed)
