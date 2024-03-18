5539e059a48871ec2c582a01949f96d7c3fdf563
Ioannis <72883899+anaktas24@users.noreply.github.com>
2024-03-18 20:01:31 +0100

    Update player.gd

--- player.gd
+++ player.gd
@@ -9,6 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -29,3 +30,10 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+	
+	#Camera Controller match position of player
+	$CameraController.position = lerp($CameraController.position,position,0.05)
+	
+	
+	
